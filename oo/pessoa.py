fb16c84ca07c7d0ba33776e15ed1dd8f1f1f64de
Igor Daher <igor_drs@hotmail.com>
2021-06-14 23:09:27 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome=nome
     def cumprimentar (self):
         return 'Hello World'
 
 if __name__ == '__main__':
-    p=Pessoa()
+    p=Pessoa('Gabriel')
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome='Igor'
+    print(p.nome)
+    print(p.idade)
